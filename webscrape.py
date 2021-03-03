c0be97bf8ac049ccc1b0fc26f7aff0d438c84849
BuzilovDorin <buzilovdorin@gmail.com>
2021-03-03 20:32:31 +0000

    commit

--- webscrape.py
+++ webscrape.py
@@ -1,61 +1,154 @@
-# steps involved:
-# 1] Script checks each week folder and compares to previous log.
-#    If a new element is found it is added to a new list or if an element is missing it is added to a second list.
-# 2] The script will then go to the provided website url, verify that the files need to be updated and do so.
-
-import json
+from requests import get, post
 import os
-from pprint import pprint
-# "Logs": [
-#     {
-#         "15/09/2020": 0,
-#         "wk3": " css.html  - upload",
-#         "wk5": " proo.html  - deleted"
-#     },
-#     {
-#         "12/09/2020": 0,
-#         "wk1": " index.html  - upload",
-#         "wk2": " index.html  - removed"
-#     }
-# ]
+import json
+from dateutil import parser
+import datetime
 
-rec_URL = "https://drive.google.com/drive/folders/1pFHUrmpLv9gEJsvJYKxMdISuQuQsd_qX"
+# Module variables to connect to moodle api:
+# Insert token and URL for your site here.
+# Mind that the endpoint can start with "/moodle" depending on your installation.
 KEY = "8cc87cf406775101c2df87b07b3a170d"
 URL = "https://034f8a1dcb5c.eu.ngrok.io"
 ENDPOINT = "/webservice/rest/server.php"
+courseid = "20"
 
 
-# Check if this files system is run for the first time
-def Files_Check():
-    with open('FilesLog.json', "r") as f:
-        data = json.load(f)
-        if data["Initialize"] == "yes":
-            # Mark it off as no so the script will no every subsequent run will not be the first time
-            # data["Initialize"] = "no"
-            with open('FilesLog.json', 'w') as file:
-                json.dump(data, file)
-            System_Init()
-        else:
-            System_Update()
+def rest_api_parameters(in_args, prefix='', out_dict=None):
+    """Transform dictionary/array structure to a flat dictionary, with key names
+    defining the structure.
+    Example usage:
+    >>> rest_api_parameters({'courses':[{'id':1,'name': 'course1'}]})
+    {'courses[0][id]':1,
+     'courses[0][name]':'course1'}
+    """
+    if out_dict == None:
+        out_dict = {}
+    if not type(in_args) in (list, dict):
+        out_dict[prefix] = in_args
+        return out_dict
+    if prefix == '':
+        prefix = prefix + '{0}'
+    else:
+        prefix = prefix + '[{0}]'
+    if type(in_args) == list:
+        for idx, item in enumerate(in_args):
+            rest_api_parameters(item, prefix.format(idx), out_dict)
+    elif type(in_args) == dict:
+        for key, item in in_args.items():
+            rest_api_parameters(item, prefix.format(key), out_dict)
+    return out_dict
 
 
-# Do this only the first time the script is run
-def System_Init():
-    upload_files = []
-    # https://github.com/mikhail-cct/ca3-test/blob/master/wk1/index.html
-    # Search the folder of which this script is placed within
-    subfolders = [f.path for f in os.scandir() if f.is_dir()]
-    files = [f.path for f in os.scandir(subfolders[0])]
+def call(fname, **kwargs):
+    """Calls moodle API function with function name fname and keyword arguments.
+    Example:
+    >>> call_mdl_function('core_course_update_courses',
+                           courses = [{'id': 1, 'fullname': 'My favorite course'}])
+    """
+    parameters = rest_api_parameters(kwargs)
+    parameters.update(
+        {"wstoken": KEY, 'moodlewsrestformat': 'json', "wsfunction": fname})
+    # print(parameters)
+    response = post(URL+ENDPOINT, data=parameters).json()
+    if type(response) == dict and response.get('exception'):
+        raise SystemError("Error calling Moodle API\n", response)
+    return response
+
+################################################
+# Rest-Api classes
+################################################
+
+
+class LocalGetSections(object):
+    """Get settings of sections. Requires courseid. Optional you can specify sections via number or id."""
+
+    def __init__(self, cid, secnums=[], secids=[]):
+        self.getsections = call('local_wsmanagesections_get_sections',
+                                courseid=cid, sectionnumbers=secnums, sectionids=secids)
+
+
+class LocalUpdateSections(object):
+    """Updates sectionnames. Requires: courseid and an array with sectionnumbers and sectionnames"""
+
+    def __init__(self, cid, sectionsdata):
+        self.updatesections = call(
+            'local_wsmanagesections_update_sections', courseid=cid, sections=sectionsdata)
+
+################################################
+# Example
+################################################
+
+
+# Get all sections of the course.
+sec = LocalGetSections(courseid)
+
+# Output readable JSON, but print only summary
+print(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
+
+# Split the section name by dash and convert the date into the timestamp, it takes the current year, so think of a way for making sure it has the correct year!
+month = parser.parse(list(sec.getsections)[1]['name'].split('-')[0])
+# Show the resulting timestamp
+print(month)
+# Extract the week number from the start of the calendar year
+print(month.strftime("%V"))
+
+#  Assemble the payload
+data = [{'type': 'num', 'section': 0, 'summary': '', 'summaryformat': 1, 'visible': 1,
+         'highlight': 0, 'sectionformatoptions': [{'name': 'level', 'value': '1'}]}]
+
+# Assemble the correct summary
+summary = '<a href="https://mikhail-cct.github.io/ca3-test/wk1/">Week 1: Introduction</a><br>'
+
+# Assign the correct summary
+data[0]['summary'] = summary
+
+# Set the correct section number
+data[0]['section'] = 1
+
+# Write the data back to Moodle
+sec_write = LocalUpdateSections(courseid, data)
+sec = LocalGetSections(courseid)
+print(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
+
+
+files_prefix = os.path.abspath("./")
+
+
+def Moodle_Updater():
     for i in os.scandir():
         if i.is_dir():
             for f in os.scandir(i.path):
-                upload_files.append(f.path)
-    pprint(upload_files)
+                print(i.path)
 
 
-def System_Update():
-    # update the files on the ngrok server
-    return None
+Moodle_Updater()
 
+# def Files():
+#     with open('FilesLog.json', "r") as f:
+#         data = json.load(f)
+#         if data["Initialize"] == "yes":
+#             # Mark it off as no so the script will no every subsequent run will not be the first time
+#             # data["Initialize"] = "no"
+#             with open('FilesLog.json', 'w') as file:
+#                 json.dump(data, file)
+#             System_Init()
+#         else:
+#             System_Update()
 
-Files_Check()
+# # Do this only the first time the script is run
+# def System_Init():
+#     upload_files = []
+#     # https://github.com/mikhail-cct/ca3-test/blob/master/wk1/index.html
+#     # Search the folder of which this script is placed within
+#     subfolders = [f.path for f in os.scandir() if f.is_dir()]
+#     files = [f.path for f in os.scandir(subfolders[0])]
+#     for i in os.scandir():
+#         if i.is_dir():
+#             for f in os.scandir(i.path):
+#                 upload_files.append(f.path)
+
+# def System_Update():
+#     # update the files on the ngrok server
+#     return None
+
+# Files_Check()
